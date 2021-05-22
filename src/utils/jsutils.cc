96bac05a5e113afb5fcf006a3ec803f2a4cf5de6
Gabriel Rodrigues <jipacoding@gmail.com>
2021-05-22 13:24:38 -0400

    feat: utils: jsutils: JS_IsNullOrUndefined

---
+++ src/utils/jsutils.cc
@@ -0,0 +1,27 @@
+/**
+ * This file is part of quickjs-gobject.
+ *
+ * quickjs-gobject is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * quickjs-gobject is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with quickjs-gobject. If not, see <https://www.gnu.org/licenses/>
+ **/
+
+#include <quickjs/quickjs.h>
+#include "utils/jsutils.hh"
+
+namespace QJSGir {
+
+bool JS_IsNullOrUndefined(JSValue value) {
+  return JS_IsNull(value) || JS_IsUndefined(value);
+}
+
+}
